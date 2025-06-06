985220698a47f69d4ebf318f1e77562baada014b
MiroslavaMoh <miroslavamoh252@gmail.com>
2025-06-06 08:50:22 -0600

    First commit

---
+++ src/test/java/com/tecsoftware/donut_app_backend/DonutAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsoftware.donut_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
